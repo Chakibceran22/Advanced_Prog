2bf737bb703d451c096ad08124f918dc5af8f16d
Chakibceran22 <grabachakib008@gmail.com>
2024-12-09 18:31:14 +0100

    finishing exo 2

---
+++ exo_2.py
@@ -0,0 +1,9 @@
+
+tempereture = int (input("Please enter the temperature in Celsius: "))
+if tempereture < 20:
+    print("It's cool !")
+    if tempereture < 10:
+        print("It's cold ")
+        if tempereture < 0:
+            print("It's freezing!! ")
+print("stay safe outside")               
